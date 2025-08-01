ec196e841cf686b821d6ce6d86d18d8bb817e3c7
Your Name <mvmohitkumar17@gmail.com>
2025-08-01 17:30:16 +0530

    completed the client document sign

--- src/components/headdocsign.jsx
+++ src/components/headdocsign.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import { useSelector } from 'react-redux'; // Import useSelector hook
+import React, { useState, useRef, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import userImage from '../assets/profile.jpg';
 import '../css/headermain.css';
 import homeIcon from '../assets/home.png';
 
 const Headdocsign = () => {
-  // Use useSelector to grab the state from the Redux store
   const userName = useSelector((state) => state.user.name);
   const masterEntityName = useSelector((state) => state.user.masterEntityName);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
   return (
     <div className='header'>
@@ -34,10 +53,37 @@ const Headdocsign = () => {
               </div>
             </div>
         </div>
-        {/* Now the name is dynamic, coming from Redux state */}
         <h3>User Name: {userName}</h3>
-        <div className="profile">
-          <img src={userImage} alt="profile" />
+        <div className="profile-container" ref={dropdownRef}>
+          <button className="profile-button" onClick={toggleDropdown}>
+            <img src={userImage} alt="profile" />
+          </button>
+          {isDropdownOpen && (
+            <div className="profile-dropdown">
+              <ul>
+                <li>
+                  <button className="dropdown-item">
+                    <span>Add Image</span>
+                  </button>
+                </li>
+                <li>
+                  <button className="dropdown-item">
+                    <span>Add New User</span>
+                  </button>
+                </li>
+                <li>
+                  <button className="dropdown-item">
+                    <span>Change Password</span>
+                  </button>
+                </li>
+                <li>
+                  <button className="dropdown-item">
+                    <span>Sign out</span>
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
       <div className="header-bottom">
@@ -45,7 +91,6 @@ const Headdocsign = () => {
           <img src={homeIcon} alt="Home" className="home-icon" />
           <b><a href="/client-portal">Client Portal <b>&gt;</b> Document signing</a></b>
         </div>
-        {/* The entity name is also dynamic */}
         <p>Master Entity name: {masterEntityName}</p>
       </div>
     </div>
